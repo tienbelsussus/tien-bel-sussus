2e3172d15b9e5305d6aefd4e063947875a33885b
tienbelsussus <trantienxdk2k7@gmail.com>
2026-01-11 19:46:29 +0700

    Add files via upload

---
+++ script3.py
@@ -0,0 +1,15 @@
+print ( " hello " )
+
+
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+c = int(input("Enter third integer: "))
+
+s = a + b + c
+p = a * b * c
+avg = s / 3
+
+print("Sum:", s)
+print("Product:", p)
+print("Average:", avg)
+
