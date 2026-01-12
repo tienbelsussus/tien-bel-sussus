e84b5969bf92196ada932239f44b7bb5fd72ce60
tienbelsussus <trantienxdk2k7@gmail.com>
2026-01-12 09:59:33 +0700

    Add files via upload

---
+++ câu 3.py
@@ -0,0 +1,7 @@
+radius = float(input("mấy radius "))
+
+
+import math
+circumference = 2 * math.pi * radius
+
+print("chu vi", circumference)
---
+++ câu 4.py
@@ -0,0 +1,8 @@
+length = float(input("chiều dài"))
+width = float(input("chiều rộng"))
+#calculate perimeter and area
+perimeter = 2*(length+width)
+area = length*width
+#result
+print("chu vi",perimeter)
+print("diện tích",area)
