353e1f278b746f5081cd390052b4215aaec7f2fa
tienbelsussus <trantienxdk2k7@gmail.com>
2026-01-12 10:13:03 +0700

    Add files via upload

---
+++ câu 5.py
@@ -0,0 +1,15 @@
+# Ask the user for three integers
+a = int(input("số 1: "))
+b = int(input("số 2: "))
+c = int(input("số 3: "))
+
+# Calculate
+tổng= a + b + c
+tích = a * b * c
+tbc = tổng / 3
+
+# results
+print("tổng:", tổng)
+print("tích:", tích)
+print("tbc:", tbc)
+
